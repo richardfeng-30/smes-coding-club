7485c8c2dfbcec5a82f73127e96a8bb670aa3ccc
richardfeng-30 <richardqfeng@gmail.com>
2024-02-06 21:09:27 -0800

    smes coding club

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-gradient-to-r from-slate-950 to-slate-800 text-white`}
+      >
+        <div className="max-w-5xl mx-4 sm:6 md:mx-8 lg:mx-auto">
+          {" "}
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
