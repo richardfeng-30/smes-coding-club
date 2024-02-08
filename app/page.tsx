4e0a5de6e56d8360ade52f64931aef471b1b949e
richardfeng-30 <richardqfeng@gmail.com>
2024-02-07 21:09:26 -0800

    umami track event

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <p className="text-xl sm:text-2xl">Welcome to the</p>
         <div className="lg:h-4"></div>
         <h1 className="text-5xl sm:text-7xl font-bold">SMES Coding Club</h1>
-
         <p className="text-slate-400 max-w-prose mt-16 text-justify sm:text-lg !leading-8">
           The Coding Club seeks to inspire and educate students in computer
           science by providing a welcoming and supportive learning environment.
@@ -21,6 +20,7 @@ export default function Home() {
             <Link
               href="https://forms.gle/4Thq4xihHCd5FCeu8"
               target="_blank"
+              data-umami-event="mailing list button"
               className="relative inline-flex items-center justify-center px-8 py-4 text-sm font-semibold text-white transition-all duration-200 bg-slate-900 ring-1 ring-white/10 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
             >
               Join our mailing list!
