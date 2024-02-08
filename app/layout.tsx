69327c77b5e4c93e59a2306bd7a7ec2aa76e1156
richardfeng-30 <richardqfeng@gmail.com>
2024-02-07 21:05:22 -0800

    use next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,11 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script
+      <Script
         async
         src="https://eu.umami.is/script.js"
         data-website-id="ba79bfe6-6e55-4f59-ab15-d4161f00d21f"
-      ></script>
+      />
       <body className={`${inter.className} bg-black text-white`}>
         <div className="max-w-7xl mx-4 sm:6 md:mx-8 lg:mx-auto">
           {" "}
