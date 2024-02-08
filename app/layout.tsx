4afd42ff83a80787ab7cbebd48e2073445d54218
richardfeng-30 <richardqfeng@gmail.com>
2024-02-07 20:54:29 -0800

    add umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://eu.umami.is/script.js"
+        data-website-id="ba79bfe6-6e55-4f59-ab15-d4161f00d21f"
+      ></script>
       <body className={`${inter.className} bg-black text-white`}>
         <div className="max-w-7xl mx-4 sm:6 md:mx-8 lg:mx-auto">
           {" "}
